0fa560583c87e6975ff337f366304cf97b091ce2
Patobeur <patobeur41@gmail.com>
2021-06-19 12:26:52 +0200

    on wire !

--- public/gameRules.js
+++ public/gameRules.js
@@ -14,6 +14,26 @@ console.log(ScreenW, ScreenH)
 
 // ------------------------------------- CLASS ----------------------
 // creating and adjusting the board to screen
+class WariaGame {
+	constructor() {
+		this.board = new Board('level1', 640, 480);
+		this.slidingRoad = new SlidingRoad('level1', 640, 480);
+
+		this.name = name;
+		this.hauteur = hauteur;
+		this.largeur = largeur;
+		this.movesize = 2 // pixels
+		this.intervalSize = 5; // microsec interval roadslider playforward
+	}
+	get_ScreenDomInfo_Once() {
+
+	}
+	AdjustBoardDomInfo() {
+
+	}
+}
+// ------------------------------------- CLASS ----------------------
+// creating and adjusting the board to screen
 class Board {
 	constructor(name, hauteur, largeur) {
 		this.divId = 'road-slider';
@@ -34,12 +54,13 @@ class Board {
 // adjusting the road (% ?? or pixels)
 // and make it move when keys [->,<-,Q or D] are pressed
 class SlidingRoad {
-	constructor(name, hauteur, largeur) {
+	constructor(name, hauteur, largeur, nbpan) {
 		this.divId = 'road-slider';
 		this.name = name;
 		this.hauteur = hauteur;
 		this.largeur = largeur;
-		this.movesize = 2 // pixels
+		this.nbPan = nbpan;
+		this.movesize = 2 // pixels move when sliding forward or backward
 		this.intervalSize = 5; // microsec interval roadslider playforward
 	}
 	get_RoadDomInfo_Once() {
@@ -70,24 +91,6 @@ class SlidingRoad {
 	}
 }
 // ------------------------------------- CLASS ----------------------
-class Waria {
-	constructor(name, divW, divH) {
-		this.xp = 0
-		this.lv = 1
-		this.heart = 3 // player heart
-		// this.health = this.heart * 100 * this.lv // player hp
-		this.name = name;
-		this.divW = divW;
-		this.divH = divH;
-	}
-	get_PlayerDomInfo_Once() {
-
-	}
-	detectKeyPress() { }
-	detectKeyUnPress() { }
-	addAction() { }
-}
-// ------------------------------------- CLASS ----------------------
 class Keyboard {
 	constructor(name, divW, divH) {
 		this.keys = {
@@ -113,3 +116,43 @@ class Keyboard {
 	detectKeyUnPress() { }
 	addAction() { }
 }
+// ------------------------------------- CLASS ----------------------
+class Collisions {
+	constructor() {
+
+	}
+}
+
+// ------------------------------------- PLAYER CLASS ---------------
+class Player {
+	constructor(name) {
+		this.xp = 0
+		this.lv = 1
+		this.heart = 3 // player heart
+		// this.health = this.heart * 100 * this.lv // player hp
+		this.name = name;
+		this.divW = 64 * 2; // pixels (fixed *2 to fit my needs for testing)
+		this.divH = 44 * 2; // pixels (fixed *2 to fit my needs for testing)
+		this.acting = "idle"; // default empty = idle {'', 'idle','run'}
+		this.facing = ""; // default empty = right {'', 'right','left'}
+		this.standing = ""; // default empty = not crouch {'', 'crouch','jump'} ???
+		// run and jump ?? or run Or jump ??
+	}
+	get_PlayerDomInfo_Once() {
+		// stuff do do
+		// then
+		display_Player_Once()
+	}
+	display_Player_Once() {
+		// display and\or create player div in DOM
+	}
+	refreshPlayerDomInfo() {
+		// set div attributs in Dom
+		// called each cycle of refreshing
+		// by Class SlidingRoad->playforward
+	}
+	detectKeyPress() { }
+	detectKeyUnPress() { }
+	addAction() { }
+}
+let PlayerOne = new Player("Waria")
