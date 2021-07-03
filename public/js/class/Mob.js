40c0c87e0dbfd1692df7f99c7c8b69965e4214b3
Pi-L <47693556+Pi-L@users.noreply.github.com>
2021-07-03 17:33:59 +0200

    [FIX] shollow copy of Mob - posx passed by ref

--- public/js/class/Mob.js
+++ public/js/class/Mob.js
@@ -1,20 +1,20 @@
 "use strict";
 
-class Mob{
-    constructor(x, name ,spawned, triggerx, posx, aoe, hp, h, w, speed, xp, hit, mode, bgimg ){
-        this.x =  x,  // starting x pos
-		this.name =  name, // mob name
-		this.spawned = spawned, // become true if spawned
-		this.triggerx = triggerx, // when x = triggerx something is trigered.. or not
-		this.posx =  posx,
-		this.aoe = aoe, // area of effect, hurting distance in pixels ( must be ratio converted )
-		this.hp = hp,
-		this.h = h, // div heigth ( ratio converted )
-		this.w = w, // div width ( ratio converted )
-		this.speed = speed, // pixel to move each scene iteration
-		this.xp = xp, // player's xp win when mob die
-		this.hit = hit, // hit damage to player
-		this.mode = mode, // 0: run to player , 1: stick to player, 2 run to end road; 
-		this.bgimg = bgimg // default background img idle
+class Mob {
+    constructor(x, name, spawned, triggerx, posx, aoe, hp, h, w, speed, xp, hit, mode, bgimg) {
+        this.x = x,  // starting x pos
+            this.name = name, // mob name
+            this.spawned = spawned, // become true if spawned
+            this.triggerx = triggerx, // when x = triggerx something is trigered.. or not
+            this.posx = [...posx],
+            this.aoe = aoe, // area of effect, hurting distance in pixels ( must be ratio converted )
+            this.hp = hp,
+            this.h = h, // div heigth ( ratio converted )
+            this.w = w, // div width ( ratio converted )
+            this.speed = speed, // pixel to move each scene iteration
+            this.xp = xp, // player's xp win when mob die
+            this.hit = hit, // hit damage to player
+            this.mode = mode, // 0: run to player , 1: stick to player, 2 run to end road; 
+            this.bgimg = bgimg // default background img idle
     }
 }
