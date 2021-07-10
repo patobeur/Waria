d444442a54540f048fcaf90d12f7839977fe92c2
Patobeur <patobeur41@gmail.com>
2021-07-10 07:55:29 +0200

    add delay between hits

--- public/js/class/Mob.js
+++ public/js/class/Mob.js
@@ -14,9 +14,9 @@ class Mob {
 			this.speed = speed, // pixel to move each scene iteration
 			this.xp = xp, // player's xp win when mob die
 			this.hit = hit, // hit damage to player
-			this.mode = mode, // 0: run to player , 1: stick to player, 2 hurt, 3 run to end road, 4 run back at player death; 
-			this.bgimg = bgimg // default background img idle
-		this.out = out // despawn if true
-		this.delay = delay // delay between hits
+			this.mode = mode, // 0: run to player , 1: stick to player, 2 hurt, 3 run to end road, 4 run back at player death
+			this.bgimg = bgimg, // default background img idle
+			this.out = out, // despawn if true
+			this.delay = delay // delay between hits
 	}
 }
